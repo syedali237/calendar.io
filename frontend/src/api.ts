c3026e6722307a6a0c42be3096106e15f135c72a
Syed Ali Ul Hasan <syedaliulhasan19@gmail.com>
2025-02-10 19:29:05 +0530

    fixed for localhost

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: "http://localhost:8000/",
-    baseURL: "https://test-repo-umuy.onrender.com/",
+    baseURL: "http://localhost:8000/",
+    // baseURL: "https://test-repo-umuy.onrender.com/",
 });
 
 api.interceptors.request.use((config) => {
